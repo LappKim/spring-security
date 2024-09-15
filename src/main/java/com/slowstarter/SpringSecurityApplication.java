8828b6bd50f1cb83b6eeb1bd63e250322629fb07
snake <snake@host.docker.internal>
2024-09-15 15:16:04 +0900

    main class 명칭을 project와 맞춤.

--- src/main/java/com/slowstarter/SpringSecurityApplication.java
+++ src/main/java/com/slowstarter/SpringSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityStudyApplication {
+public class SpringSecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityStudyApplication.class, args);
+        SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
 }
